0c5bd7c458681404e23dcefcc88a96703b28e936
Pedro Gil Candeias <pgscandeias@gmail.com>
2016-03-06 18:26:30 +0100

    DTO superclass for commands and events

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -2,77 +2,10 @@
 
 namespace Candeias\Users\Command;
 
-use DateTime;
-use Candeias\Users\Entity\User;
+use Candeias\Users\Common\DTO;
 
-class Command
+class Command extends DTO
 {
     const REGISTER = 'register';
     const UPDATE = 'update';
-
-    /**
-     * @var string
-     */
-    protected $type;
-
-    /**
-     * @var DateTime
-     */
-    protected $time;
-
-    /**
-     * @var User
-     */
-    protected $user;
-
-    /**
-     * @var array
-     */
-    protected $data;
-
-    /**
-     * @param string   $type
-     * @param DateTime $time
-     * @param User     $user
-     * @param array    $data
-     */
-    public function __construct(
-        $type,
-        DateTime $time,
-        User $user,
-        array $data = []
-    ) {
-        $this->type = $type;
-        $this->time = $time;
-        $this->user = $user;
-        $this->data = $data;
-    }
-
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    public function getTime()
-    {
-        return $this->time;
-    }
-
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    public function getData()
-    {
-        return $this->data;
-    }
-
-    public function get($key)
-    {
-        return isset($this->data[$key]) ?
-            $this->data[$key] :
-            null
-        ;
-    }
 }
--- src/Event/Event.php
+++ src/Event/Event.php
@@ -2,76 +2,9 @@
 
 namespace Candeias\Users\Event;
 
-use DateTime;
-use Candeias\Users\Entity\User;
+use Candeias\Users\Common\DTO;
 
-class Event
+class Event extends DTO
 {
     const REGISTERED = 'registered';
-
-    /**
-     * @var string
-     */
-    protected $type;
-
-    /**
-     * @var DateTime
-     */
-    protected $time;
-
-    /**
-     * @var User
-     */
-    protected $user;
-
-    /**
-     * @var array
-     */
-    protected $data;
-
-    /**
-     * @param string   $type
-     * @param DateTime $time
-     * @param User     $user
-     * @param array    $data
-     */
-    public function __construct(
-        $type,
-        DateTime $time,
-        User $user,
-        array $data = []
-    ) {
-        $this->type = $type;
-        $this->time = $time;
-        $this->user = $user;
-        $this->data = $data;
-    }
-
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    public function getTime()
-    {
-        return $this->time;
-    }
-
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    public function getData()
-    {
-        return $this->data;
-    }
-
-    public function get($key)
-    {
-        return isset($this->data[$key]) ?
-            $this->data[$key] :
-            null
-        ;
-    }
 }
